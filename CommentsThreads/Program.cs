72f946d0946c5280d1a05dae9606086fa21d0d96
nimashad2 <nimashad2@gmail.com>
2018-10-02 10:04:30 -0700

    first comment

---
+++ CommentsThreads/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Threading.Tasks;
+
+namespace CommentsThreads
+{
+    internal class Program
+    {
+        [STAThread]
+        public void Main(string[] args)
+        {
+            Console.WriteLine("YouTube Data API: Comments");
+            Console.WriteLine("========================");
+
+            try
+            {
+                new Program().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            //Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyAh8CTizm1Qq7WnvO45YU_8owLzXCBwCc4",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var req = youtubeService.CommentThreads.List("snippet");
+            req.VideoId = "XCgk50WY2nc";
+            req.TextFormat = CommentThreadsResource.ListRequest.TextFormatEnum.PlainText;
+            var res = req.Execute();
+
+            var result = await req.ExecuteAsync();
+
+            string a = "";
+        }
+
+    }
+}
---
+++ Google.Apis.YouTube.Samples.Search/RootObject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class AuthorChannelId
+    {
+        public string value { get; set; }
+    }
+
+    public class Snippet2
+    {
+        public AuthorChannelId authorChannelId { get; set; }
+        public string authorChannelUrl { get; set; }
+        public string authorDisplayName { get; set; }
+        public string authorProfileImageUrl { get; set; }
+        public bool canRate { get; set; }
+        public object channelId { get; set; }
+        public int likeCount { get; set; }
+        public object moderationStatus { get; set; }
+        public object parentId { get; set; }
+        public DateTime publishedAt { get; set; }
+        public string textDisplay { get; set; }
+        public string textOriginal { get; set; }
+        public DateTime updatedAt { get; set; }
+        public string videoId { get; set; }
+        public string viewerRating { get; set; }
+        public object ETag { get; set; }
+    }
+
+    public class TopLevelComment
+    {
+        public string etag { get; set; }
+        public string id { get; set; }
+        public string kind { get; set; }
+        public Snippet2 snippet { get; set; }
+    }
+
+    public class Snippet
+    {
+        public bool canReply { get; set; }
+        public object channelId { get; set; }
+        public bool isPublic { get; set; }
+        public TopLevelComment topLevelComment { get; set; }
+        public int totalReplyCount { get; set; }
+        public string videoId { get; set; }
+        public object ETag { get; set; }
+    }
+
+    public class Item
+    {
+        public string etag { get; set; }
+        public string id { get; set; }
+        public string kind { get; set; }
+        public object replies { get; set; }
+        public Snippet snippet { get; set; }
+    }
+
+    public class PageInfo
+    {
+        public int resultsPerPage { get; set; }
+        public int totalResults { get; set; }
+        public object ETag { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string etag { get; set; }
+        public object eventId { get; set; }
+        public List<Item> items { get; set; }
+        public string kind { get; set; }
+        public string nextPageToken { get; set; }
+        public PageInfo pageInfo { get; set; }
+        public object tokenPagination { get; set; }
+        public object visitorId { get; set; }
+    }
+
